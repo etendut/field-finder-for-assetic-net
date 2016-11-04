d34a9d1f7b9aadaaf083e235c7e2950d1c73dd1e
Lawrie George <lgeorge@Assetic.office>
2016-11-04 19:38:38 +1100

    fix no context bug

--- src/js/pageScripts/getContext.js
+++ src/js/pageScripts/getContext.js
@@ -1,6 +1,8 @@
 setTimeout(function() {
     /* Example: Send data from the page to your Chrome extension */
-    document.dispatchEvent(new CustomEvent('asseticExtension_context', {
-        detail: context
-    }));
+    if (context) {
+        document.dispatchEvent(new CustomEvent('asseticExtension_context', {
+            detail: context
+        }));
+    }
 }, 0);
