54dabc7208442d5801aeb95fd250da6e72913045
etendut <etendut@gmail.com>
2017-01-03 13:57:45 -0500

    Export Grid

--- src/js/popup.js
+++ src/js/popup.js
@@ -7,16 +7,34 @@ $(function() {
         $(".mdl-layout__drawer.is-visible").removeClass("is-visible")
         $(".mdl-layout__obfuscator.is-visible").removeClass("is-visible")
         var t = $(e.currentTarget).text();
-        switch (t) {
-            case "help_outlineHelp":
-                showHelp(helpstage);
-                helpstage = helpstage >= 2 ? 1 : helpstage += 1;
-                break;
+        if (t && t.indexOf("help_outline") > -1) {
+            showHelp(helpstage);
+            helpstage = helpstage >= 2 ? 1 : helpstage += 1;
+            return;
+        } else if (t && t.indexOf("cloud_download") > -1) {
+            var data = $('#searchGrid').jsGrid('option', 'data');
 
-            default:
-                location.reload();
-                break;
+            if (data == '')
+                return;
+
+            var fields = $('#searchGrid').jsGrid('option', 'fields');
+            //extract labels and names
+            var columnLabels = [];
+            var columns = [];
+
+            $.each(fields, function(i, obj) {
+                columnLabels.push([obj.title]);
+                columns.push([obj.name]);
+            });
+
+
+
+            JSONToCSVConvertor(data, "Field Finder Grid Export", columns, columnLabels);
+        } else {
+            location.reload();
         }
+
+
     });
 })
 
@@ -386,6 +404,7 @@ function loadSearchGrid() {
             { name: "label", title: "Control", type: "text", width: 150 },
             { name: "help", title: "Control Help", type: "text", width: 150 },
             { name: "type", title: "Control Type", type: "text", width: 100, sorting: false },
+            { name: "fieldLength", title: "Length", type: "text", width: 20, sorting: false },
             { name: "group", title: "Control Group", type: "text", width: 100 },
             {
                 name: "inCurrentCategory",
@@ -429,4 +448,88 @@ function loadCategoryGrid() {
             { name: "categoryLabel", title: "Category", type: "text" },
         ]
     });
+}
+
+function JSONToCSVConvertor(JSONData, ReportTitle, ColumnNames, ColumnLabels) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+    var CSV = '';
+    //Set Report title in first row or line
+
+    // CSV += ReportTitle + '\r\n\n';
+    debugger;
+    //This condition will generate the Label/Header
+    if (ColumnLabels) {
+        var row = "";
+
+        //This loop will extract the label from 1st index of on array
+        for (var index in ColumnLabels) {
+
+            //Now convert each value to string and comma-seprated
+            row += ColumnLabels[index] + ',';
+        }
+
+        row = row.slice(0, -1);
+
+        //append Label row with line break
+        CSV += row + '\r\n';
+    }
+
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+
+        //2nd loop will extract each column and convert it in string comma-seprated
+        if (ColumnNames) {
+            for (var column in ColumnNames) {
+                try {
+                    var columnData = arrData[i][ColumnNames[column]];
+                    row += '"' + (columnData ? columnData : "") + '",';
+                } catch (error) {
+                    //do nothing
+                }
+            }
+        } else {
+            for (var index in arrData[i]) {
+                row += '"' + arrData[i][index] + '",';
+            }
+        }
+
+        row.slice(0, row.length - 1);
+
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {
+        alert("Invalid data");
+        return;
+    }
+
+    //Generate a file name
+    var fileName = "";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g, "_");
+
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
